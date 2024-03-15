3a5af4cc99d271fbc5cf50f0311833aa2f70db7c
akhilesh.sooji <akhilesh.sooji@wizardtales.com>
2024-03-15 19:27:04 +0530

    authentication

--- tmp/auth.js
+++ tmp/auth.js
@@ -6,12 +6,14 @@ import Promise from 'bluebird';
 export default {
   request: async (request, { username, password, ip }) => {
     const authenticator = request.sp;
+    console.log('Helloo ', authenticator);
     // const { rows } = await pool.query(
     //  'SELECT * FROM "user" WHERE username = $1 AND password = $2',
     //  [username, password]
     // );
 
-    const result = await authenticator.auth(username, ip, password);
+    const result = await authenticator.getAuth(username, ip, password);
+    console.log('result', result);
     if (result.auth === true && result.code === 200) {
       const { pool } = request.server.plugins.pg;
       const {
