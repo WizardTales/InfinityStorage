48c0ffdc68f7e0af88b93f6a81f245a8c2086a59
DasWizardTales <sylvester.das@wizardtales.com>
2024-05-23 12:02:16 +0530

    chore: cleanup

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ import multipart from '@fastify/multipart';
 import upload from './lib/controllers/upload.js';
 import routes from './lib/routes/index.js';
 import Promise from 'bluebird';
-import storageService from './lib/services/storage.js';
 import cors from '@fastify/cors';
 import config from './config.js';
 import CRDB from 'crdb-pg';
 import DBMigrate from 'db-migrate';
-import fastifySession from '@fastify/session';
 import fastifyCookie from '@fastify/cookie';
 import spPlugin from './lib/plugins/sp.js';
 import Iron from '@hapi/iron';
-
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 config.s3.port = Number(config.s3.port);
 if (typeof config.s3.useSSL === 'string') {
@@ -43,26 +39,7 @@ fastify.register(fastifyCookie, {
   parseOptions: config.cookies.config
 });
 
-// fastify.register(fastifySession, {
-//   secret: '2b2fd0faa75e6a9f99d513911e7a5cb5802ca65b635bf587e3e784eb51e051a4', // Secret testing  purposes only!
-//   cookie: {
-//     secure: false
-//   }
-// });
-
 fastify.addHook('preHandler', spPlugin);
-
-fastify.addHook('onRequest', async (request, reply) => {
-  // if (request.session.user) {
-  //   request.user = request.session.user;
-  //   const { pool } = request.server.pg;
-  //   const { msg: storage } = await storageService.getStorageById(
-  //     pool,
-  //     request.user.id
-  //   );
-  //   request.storage = storage;
-  // }
-});
 
 // Routes
 fastify.register(routes);
