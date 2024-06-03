2301ff06c8597a96f4fe6e8269280778241e8e64
DasWizardTales <sylvester.das@wizardtales.com>
2024-06-03 20:18:08 +0530

    chore: remove the 2nd part encryption
    Since that is to be done on the browser side.

--- test/crypto.spec.js
+++ test/crypto.spec.js
@@ -1,31 +1,15 @@
 import { describe, it } from 'mocha';
-import {
-  encryptPart1,
-  decryptPart1,
-  encryptPart2,
-  decryptPart2
-} from '../lib/services/crypto.js';
+import { encryptPart, decryptPart } from '../lib/services/crypto.js';
 import assert from 'assert';
 
 describe('Crypto Service', function () {
   it('should encrypt and decrypt a password 1st part', function () {
     const part = Math.random().toString().substring(0, 16);
 
-    const encrypted = encryptPart1(part);
+    const encrypted = encryptPart(part);
     assert.notEqual(part, encrypted);
 
-    const decrypted = decryptPart1(encrypted);
-    assert.equal(part, decrypted);
-  });
-
-  it('should encrypt and decrypt a password 2nd part', function () {
-    const part = Math.random().toString().substring(0, 16);
-    const userPass = Math.random().toString(36);
-
-    const encrypted = encryptPart2(part, userPass);
-    assert.notEqual(part, encrypted);
-
-    const decrypted = decryptPart2(encrypted, userPass);
+    const decrypted = decryptPart(encrypted);
     assert.equal(part, decrypted);
   });
 });
