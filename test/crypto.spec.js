34d79a2cd3c3d5f7cc84de2e7ffca7c535a3c237
DasWizardTales <sylvester.das@wizardtales.com>
2024-06-04 11:26:09 +0530

    chore: crypto final

--- test/crypto.spec.js
+++ test/crypto.spec.js
@@ -4,12 +4,24 @@ import assert from 'assert';
 
 describe('Crypto Service', function () {
   it('should encrypt and decrypt a password 1st part', function () {
-    const part = Math.random().toString().substring(0, 16);
+    const part = '4i77cg3ah4p96eez';
 
     const encrypted = encryptPart(part);
     assert.notEqual(part, encrypted);
 
     const decrypted = decryptPart(encrypted);
-    assert.equal(part, decrypted);
+    assert.strictEqual(part, decrypted);
+  });
+
+  it('should throw error on invalid part length on encryption', function () {
+    const invalidPart = 'asdasc';
+
+    assert.throws(() => encryptPart(invalidPart));
+  });
+
+  it('should throw error on invalid string input on decryption', function () {
+    const invalidString = 'asdasc';
+
+    assert.throws(() => decryptPart(invalidString));
   });
 });
